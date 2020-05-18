72eab52d4fde5e9fd0cb82028e0e8356df789f66
Lee Briggs <lee@leebriggs.co.uk>
2020-05-18 11:45:20 -0700

    Rework to support any GitHub release

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,79 @@
 import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
+import * as github from "@actions/github";
 import * as os from "os";
 
 const mkdirp = require("mkdirp-promise");
 
 async function run() {
     try {
-        const version = core.getInput("tf2pulumi-version");
-        if (version == undefined) {
-            core.setFailed("tf2pulumi-version not specified");
+
+        // set up auth/environment
+        const token = process.env['GITHUB_TOKEN']
+        if (!token) {
+            throw new Error(
+                `No GitHub token found`
+            )
+        }
+        const octokit: github.GitHub = new github.GitHub(token)
+
+        const repo = core.getInput("repo");
+        if (!repo) {
+            throw new Error(
+                `Repo was not specified`
+            )
             return;
         }
 
-        const destination = "/home/runner/.tf2pulumi";
+        const tag = core.getInput("tag");
+        if (!tag) {
+            throw new Error(
+                `Tag not specified`
+            )
+        }
+
+        const [owner, project] = repo.split("/")
+
+        const destination = `/home/runner/.${project}`;
         await mkdirp(destination);
 
         let osPlatform = os.platform();
         if (osPlatform != "linux" && osPlatform != "darwin") {
-            core.setFailed("Unsupported operating system - tf2pulumi is only released for Darwin and Linux");
+            core.setFailed(`Unsupported operating system - $this action is only released for Darwin and Linux`);
             return;
         }
+        
+        let getReleaseUrl;
+        if (tag === "latest") {
+            getReleaseUrl = await octokit.repos.getLatestRelease({
+                owner: owner,
+                repo: project,
+            })
+        } else {
+            getReleaseUrl = await octokit.repos.getReleaseByTag({
+                owner: owner,
+                repo: project,
+                tag: tag,
+            })
+        }
 
-        const url = `https://github.com/pulumi/tf2pulumi/releases/download/v${version}/tf2pulumi-v${version}-${osPlatform}-x64.tar.gz`
-        const tf2pulumiPath = await tc.downloadTool(url);
-        const extractedPath = await tc.extractTar(tf2pulumiPath, destination);
+        let re = new RegExp(`${osPlatform}.*tar.gz`)
+        let asset = getReleaseUrl.data.assets.find(obj => {
+            return re.test(obj.name)
+        })
+
+        if (!asset ) {
+            const found = getReleaseUrl.data.assets.map(f => f.name)
+            throw new Error(
+                `Could not find a release for ${tag}. Found: ${found}`
+            )
+        }
+
+        const url = asset.browser_download_url
+
+        console.log(`Downloading ${project} from ${url}`)
+        const binPath = await tc.downloadTool(url);
+        const extractedPath = await tc.extractTar(binPath, destination);
 
         core.addPath(extractedPath);
     } catch (error) {
